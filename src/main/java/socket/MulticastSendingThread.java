bc47fc985e2e844bffcb06aa7e72a625531f4015
tamami <tamami.oka@gmail.com>
2017-05-11 22:10:33 +0700

    finishing function

--- src/main/java/socket/MulticastSendingThread.java
+++ src/main/java/socket/MulticastSendingThread.java
@@ -1,5 +1,8 @@
 package socket;
 
+import java.io.IOException;
+import java.net.*;
+
 /**
  * Created by tamami on 11/05/17.
  */
@@ -10,6 +13,23 @@ public class MulticastSendingThread extends Thread {
     public MulticastSendingThread(byte[] bytes) {
         super("MulticastSendingThread");
         messageBytes = bytes;
+    }
+
+    public void run() {
+        try {
+            DatagramSocket socket = new DatagramSocket(SocketMessengerConstants.MULTICAST_SENDING_PORT);
+            InetAddress group = InetAddress.getByName(SocketMessengerConstants.MULTICAST_ADDRESS);
+            DatagramPacket packet = new DatagramPacket(messageBytes, messageBytes.length, group,
+                SocketMessengerConstants.MULTICAST_LISTENING_PORT);
+            socket.send(packet);
+            socket.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
